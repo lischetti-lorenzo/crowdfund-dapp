5ef1dc08b5c8e0bd98c5ab296f75fb30ecd8b606
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2022-01-19 22:38:05 -0300

    Setting up web3

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from './web3';
+import CampaignFactory from './build/CampaignFactory.json';
+
+const contract = new web3.eth.Contract(
+  JSON.parse(CampaignFactory.interface),
+  '0x65ec3aaCb87A8B96942870c5EdD966E15eB1A34E'
+);
+
+export default contract;
