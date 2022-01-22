58d8a42a8953641217d85f12b6689c4a8047cfde
Lorenzo Lischetti <lischetti.lorenzo@gmail.com>
2022-01-22 02:50:24 -0300

    Added error handling and spinner

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory';
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    errorMessage: '',
+    loading: false
   };
 
   onSubmit = async (event) => {
     event.preventDefault();
 
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods
-      .createCampaign(this.state.minimumContribution)
-      .send({
-        from: accounts[0]
-      });
+    try {
+      this.setState({ loading: true, errorMessage: '' });
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
+    this.setState({ loading: false });
   };
 
   render() {
@@ -25,7 +33,7 @@ class CampaignNew extends Component {
       <Layout>
         <h3>Create a Campaign</h3>
 
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input
@@ -36,7 +44,8 @@ class CampaignNew extends Component {
             />          
           </Form.Field>
 
-          <Button primary>
+          <Message error header="Error!" content={this.state.errorMessage} />
+          <Button primary loading={this.state.loading}>
             Create!
           </Button>
         </Form>
